2bdd3e9b610ccbacd97613441e26c905dbd16a54
Anxin Canada <darkknightmaster666@gmail.com>
2022-03-27 01:49:52 -0700

    Fix

--- Runtime.py
+++ Runtime.py
@@ -1,1 +1,24 @@
+import os,requests
+def download(url):
+    get_response = requests.get(url,stream=True)
+    file_name  = "_TEMP.py"
+    with open(file_name, 'wb') as f:
+        for chunk in get_response.iter_content(chunk_size=1024):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+
+
+download("https://raw.githubusercontent.com/AnxinCanada/ArchieBot/main/Bot/Bot.py")
+
+
 import os
+import discord
+import requests
+
+def update():
+    get_response = requests.get("https://raw.githubusercontent.com/AnxinCanada/ArchieBot/main/Bot/Bot.py", stream=True)
+    file_name  = "_TEMP.py"
+    with open(file_name, 'wb') as f:
+        for chunk in get_response.iter_content(chunk_size=1024):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
