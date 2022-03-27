58272c5736a305ee5a226821c74db84a77641f37
Anxin Canada <darkknightmaster666@gmail.com>
2022-03-27 09:31:22 -0700

    Yes

--- Runtime.py
+++ Runtime.py
@@ -1,27 +1,14 @@
 import os
+import sys
 import discord
-import subprocess
 from discord.ext import commands
 
 Client = commands.Bot(command_prefix="!", help_command=None)
 
-@Client.command(aliases=["reboot", "restart"])
-async def update(ctx):
-    works = 0
-    try:
-        if ctx.author.id == 746446670228619414:
-            works = 1
-    except Exception:
-        works = 0
-    if works:
-        try:
-            p.terminate()
-        except Exception:
-            pass
+async def update():
         await Client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="SERVER REBOOT"))
         try:
             os.system("mv TEMP/assets assets")
-            os.system("rm -r TEMP")
         except Exception:
             pass
         os.system("svn checkout https://github.com/AnxinCanada/ArchieBot/trunk/Bot TEMP")
@@ -33,10 +20,11 @@ async def update(ctx):
             os.system("cp Bot/Login.TOKEN TEMP/Login.TOKEN")
         except Exception:
             pass
-        p = subprocess.Popen(['python', 'TEMP/Bot.py'])
+        os.system("python3 TEMP/Bot.py")
+        sys.exit(0)
 
 @Client.event
 async def on_ready():
-    await update(Client.get_guild(957246253157191711).get_channel(957258191459213322))
+    await update()
 
 Client.run(open("Bot/Login.TOKEN").read())
