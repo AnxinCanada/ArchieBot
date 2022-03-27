b71c15fa769c84724637dff69d902767fbf1a213
Anxin Canada <darkknightmaster666@gmail.com>
2022-03-27 02:11:32 -0700

    Yes

--- Runtime.py
+++ Runtime.py
@@ -11,14 +11,20 @@ def download(url):
 download("https://raw.githubusercontent.com/AnxinCanada/ArchieBot/main/Bot/Bot.py")
 
 
+from time import sleep
 import os
 import discord
 import requests
+import shutil
 
 def update():
-    get_response = requests.get("https://raw.githubusercontent.com/AnxinCanada/ArchieBot/main/Bot/Bot.py", stream=True)
-    file_name  = "_TEMP.py"
-    with open(file_name, 'wb') as f:
-        for chunk in get_response.iter_content(chunk_size=1024):
-            if chunk: # filter out keep-alive new chunks
-                f.write(chunk)
+    try:
+        os.system("mv TEMP/assets assets")
+    except Exception:
+        pass
+    os.system("svn checkout https://github.com/AnxinCanada/ArchieBot/trunk/Bot TEMP")
+    try:
+        os.system("mv assets TEMP/assets")
+    except Exception:
+        pass
+update()
